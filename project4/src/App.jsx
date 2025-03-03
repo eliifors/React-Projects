f1e9fe0d947b85b97f170b4b0fe1382e4cd68f3f
eliifors <elif.ors@ogr.sakarya.edu.tr>
2025-03-03 16:54:50 +0300

    feat: add Course, Loading, and Courses components; integrate course fetching with API

--- project4/src/App.jsx
+++ project4/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Courses from "./Courses";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchCourses = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("http://localhost:3000/courses");
+      setCourses(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {loading ? <loading /> : <Courses courses={courses} />}
+    </div>
+  );
 }
 
-export default App
+export default App;
