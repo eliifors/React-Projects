0d43f2801da6a1d7936142b93461159a49c4b83b
eliifors <elif.ors@ogr.sakarya.edu.tr>
2025-03-04 15:58:31 +0300

    feat: implement calculator functionality with context API; remove unused styles

--- project6/src/App.jsx
+++ project6/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React, { useEffect, useReducer } from "react";
+import Calculate from "./Calculate";
+
+export const NumberContext = React.createContext();
+
+const initialValue = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialValue;
+    default:
+      return state;
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, dispatch] = useReducer(reducer, initialValue);
 
+  useEffect(() => {
+    console.log("render oldu...");
+  }, [count]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <NumberContext.Provider value={{ count: count, dispatch: dispatch }}>
+        <Calculate />
+      </NumberContext.Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
