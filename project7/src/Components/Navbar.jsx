bbe36cefa938e2cd0ff5344db8e4a91d51810228
eliifors <elif.ors@ogr.sakarya.edu.tr>
2025-03-05 16:00:30 +0300

    feat: add routing with react-router-dom; implement Home, AboutUs, Mission, Navbar, and WrongPath components

---
+++ project7/src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+function Navbar() {
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/aboutUs">About</NavLink>
+    </nav>
+  );
+}
+
+export default Navbar;
