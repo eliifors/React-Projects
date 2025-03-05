43e59266e8d9405baf43a9b82345dc2ed984bce0
eliifors <elif.ors@ogr.sakarya.edu.tr>
2025-03-05 21:05:52 +0300

    feat: add History component and nested routes; update Navbar for navigation

--- project7/src/Components/Navbar.jsx
+++ project7/src/Components/Navbar.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 function Navbar() {
   return (
-    <nav>
+    <nav className="mainNav">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/aboutUs">About</NavLink>
+      <NavLink to="/history">History</NavLink>
     </nav>
   );
 }
