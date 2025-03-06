a36a394a5e871089b72c766b3aee91f7e80294f0
eliifors <elif.ors@ogr.sakarya.edu.tr>
2025-03-06 21:58:27 +0300

    refactor: remove unused addItem and removeItem reducers from cartSlice

--- courseApp/src/Control/cartSlice.jsx
+++ courseApp/src/Control/cartSlice.jsx
@@ -11,21 +11,7 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addItem: (state, action) => {
-      state.cartItems.push(action.payload);
-      state.quantity += 1;
-      state.total += action.payload.price;
-    },
-    removeItem: (state, action) => {
-      const index = state.cartItems.findIndex(
-        (item) => item.id === action.payload.id
-      );
-      if (index !== -1) {
-        state.total -= state.cartItems[index].price;
-        state.cartItems.splice(index, 1);
-        state.quantity -= 1;
-      }
-    },
+
   },
 });
 
