e8ec20b1fc02c08daa2dceb2f55e4ff1d59a4841
eliifors <elif.ors@ogr.sakarya.edu.tr>
2025-03-06 21:58:38 +0300

    refactor: simplify initial state in cartSlice by setting static quantity and total values

--- courseApp/src/Control/cartSlice.jsx
+++ courseApp/src/Control/cartSlice.jsx
@@ -1,19 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
-import courseItems from "../courseItems";
+import { createSlice } from '@reduxjs/toolkit';
+import courseItems from '../courseItems';
 
 const initialState = {
   cartItems: courseItems,
-  quantity: courseItems.length,
-  total: courseItems.reduce((total, item) => total + item.price, 0),
+  quantity: 5,
+  total: 0,
 };
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState,
-  reducers: {
-
-  },
 });
 
-export const { addItem, removeItem } = cartSlice.actions;
+// console.log(cartSlice);
+
 export default cartSlice.reducer;
