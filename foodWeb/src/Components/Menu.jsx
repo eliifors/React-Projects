d69fec53dd3490aafa729c32c2cdc8365f456e35
eliifors <elif.ors@ogr.sakarya.edu.tr>
2025-03-06 07:11:52 +0300

    feat: enhance About, Contact, Home, and Menu components with styling and functionality; add MenuItem component and data helper

--- foodWeb/src/Components/Menu.jsx
+++ foodWeb/src/Components/Menu.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Data } from "../Helpers/Data";
+import MenuItem from "./MenuItem";
+import "../Styles/Menu.css";
 
-function Menu() {
-  return <div>Menu</div>;
-}
+const Menu = () => {
+  return (
+    <div className="menu">
+      <h1 className="menuTitle">Burgerlerimiz</h1>
+      <div className="menuList">
+        {Data.map((menuItem, key) => {
+          return (
+            <MenuItem
+              key={key}
+              image={menuItem.image}
+              name={menuItem.name}
+              content={menuItem.content}
+              price={menuItem.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
