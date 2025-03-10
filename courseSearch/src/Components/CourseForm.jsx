8be296017d89f0640afa2882b16d3c5fb1923892
eliifors <elif.ors@ogr.sakarya.edu.tr>
2025-03-11 02:15:32 +0300

    feat: implement course addition functionality; update CourseForm and CourseList components, and enhance formSlice and courseSlice for state management

--- courseSearch/src/Components/CourseForm.jsx
+++ courseSearch/src/Components/CourseForm.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { use } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  changeName,
+  changeDescription,
+  changeCost,
+} from "../store/Slices/formSlice";
+import { addCourse } from "../store/Slices/courseSlice";
 
 function CourseForm() {
+  const dispatch = useDispatch();
   const { name, description, cost } = useSelector((state) => {
     return {
       name: state.form.name,
@@ -10,27 +17,50 @@ function CourseForm() {
     };
   });
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addCourse({ name, description, cost }));
+  };
   console.log(name, description, cost);
 
   return (
     <div className="courseForm panel">
-      <h4 className="subTitle is-3">Kurs Ekle</h4>
-      <form>
+      <h4 className="title is-2 is-spaced">KURS EKLE</h4>
+      <form onSubmit={handleSubmit}>
         <div className="field-group">
           <div className="field">
             <label className="label">Ad</label>
-            <input className="input is-expanded" />
+            <input
+              className="input is-expanded"
+              onChange={(event) => {
+                dispatch(changeName(event.target.value));
+              }}
+              value={name}
+            />
           </div>
           <div className="field">
             <label className="label">Açıklama</label>
-            <textarea className="input is-expanded" />
+            <textarea
+              className="input is-expanded"
+              onChange={(event) => {
+                dispatch(changeDescription(event.target.value));
+              }}
+              value={description}
+            />
           </div>
           <div className="field">
             <label className="label">Fiyat</label>
-            <input className="input is-expanded" typeof="number" />
+            <input
+              className="input is-expanded"
+              typeof="number"
+              onChange={(event) => {
+                dispatch(changeCost(parseInt(event.target.value)));
+              }}
+              value={cost}
+            />
           </div>
           <div className="field">
-            <button className="button is-danger">Kaydet</button>
+            <button class="button is-danger is-dark">Kaydet</button>
           </div>
         </div>
       </form>
--- courseSearch/src/store/Slices/formSlice.jsx
+++ courseSearch/src/store/Slices/formSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addCourse } from "./courseSlice";
 
 const formSlice = createSlice({
   name: "form",
@@ -7,7 +8,26 @@ const formSlice = createSlice({
     description: "",
     cost: 0,
   },
-  reducers: {},
+  reducers: {
+    changeName(state, action) {
+      state.name = action.payload;
+    },
+    changeDescription(state, action) {
+      state.description = action.payload;
+    },
+    changeCost(state, action) {
+      state.cost = action.payload;
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(addCourse, (state, action) => {
+      state.name = "";
+      state.description = "";
+      state.cost = 0;
+    });
+  },
 });
+
+export const { changeName, changeDescription, changeCost } = formSlice.actions;
 
 export const formReducer = formSlice.reducer;
