de85018a2929bf87ab88c68755731809d03b1145
eliifors <elif.ors@ogr.sakarya.edu.tr>
2025-03-13 07:26:39 +0300

    feat: implement user interaction features; add expandable panel for user details, integrate album list, and enable user removal functionality

--- personsApp/src/Components/UsersListItem.jsx
+++ personsApp/src/Components/UsersListItem.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumList from "./AlbumList";
+import { RiDeleteBin5Fill } from "react-icons/ri";
+import { useRemoveUserMutation } from "../store";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function UsersListItem({ user }) {
-  return <div>{user.name}</div>;
+  const [removeUser, results] = useRemoveUserMutation();
+  const handleClick = () => {
+    removeUser(user);
+  };
+  const header = (
+    <>
+      <button
+        style={{ marginRight: "30px", border: "none", cursor: "pointer" }}
+        onClick={handleClick}
+      >
+        {results.isLoading ? (
+          <CircularProgress style={{ width: "20px", height: "20px" }} />
+        ) : (
+          <RiDeleteBin5Fill />
+        )}
+      </button>
+      {user.name}
+    </>
+  );
+  return (
+    <div>
+      <ExpandablePanel header={header}>
+        <AlbumList user={user} />
+      </ExpandablePanel>
+    </div>
+  );
 }
 
 export default UsersListItem;
